faf0b770afbc417933a465871f890a2f34bf6e7b
Will Norvelle <sir.aero.dev@gmail.com>
2014-09-12 09:33:18 -0400

    Matrix now can actually make a matrix, sorta

--- Matrix.py
+++ Matrix.py
@@ -1,8 +1,4 @@
 #this is to test, expirament, and maybe implement a method of interacting with matrices
-i = 0
-j = 0
-matrixSub1 = [1,2,3]
-matrixSub2 = [4,5,6]
-matrix = ([matrixSub1, matrixSub2]), ([matrixSub1, matrixSub2])
-matrix2 = ([3],[6])
-matrix2[1][1] = 1
+matrix = [[0 for x in range(5)] for x in range(5)]
+matrix[0][0] = 1
+matrix[1][4] = 2
