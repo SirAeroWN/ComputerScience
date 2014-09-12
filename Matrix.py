21edcaad7cfae59f4a9cdb666e9439ca26446e29
Will Norvelle <sir.aero.dev@gmail.com>
2014-09-11 20:34:47 -0400

    added nearly empty matrix file

---
+++ Matrix.py
@@ -0,0 +1,8 @@
+#this is to test, expirament, and maybe implement a method of interacting with matrices
+i = 0
+j = 0
+matrixSub1 = [1,2,3]
+matrixSub2 = [4,5,6]
+matrix = ([matrixSub1, matrixSub2]), ([matrixSub1, matrixSub2])
+matrix2 = ([3],[6])
+matrix2[1][1] = 1
