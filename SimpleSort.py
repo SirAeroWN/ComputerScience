0056fa82175b1dce372806f615d4eddd14cd6807
Will Norvelle <sir.aero.dev@gmail.com>
2014-09-18 09:16:52 -0400

    made mor efficient

--- SimpleSort.py
+++ SimpleSort.py
@@ -1,25 +1,21 @@
 def displaySortedNumbers(num1, num2, num3):
-    first = num1
-    second = num2
-    third = num3
+    if(num1 > num2):
+        temp = num1
+        num1 = num2
+        num2 = temp
+    if(num1 > num3):
+        temp = num1
+        num1 = num3
+        num3 = temp
+    if(num2 > num3):
+        temp = num2
+        num2 = num3
+        num3 = temp
 
-    if(num2 < num1 and num2 < num3):
-        first = num2
-    elif(num3 < num1 and num3 < num2):
-        first = num3
-
-    if((num1 < num2 and num1 > num3) or (num1 > num2 and num1 < num3)):
-        second = num1
-    elif((num3 < num2 and num3 > num1) or (num3 > num2 and num3 < num1)):
-        second = num3
-
-    if(num1 > num2 and num1 > num3):
-        third = num1
-    elif(num2 > num1 and num2 > num3):
-        third = num2
-
-    print("The sorted numbers are", first, second, third)
-    return
+    return num1, num2, num3
 
 firstUser, secondUser, thirdUser = eval(input("Enter three numbers seperated by commas: "))
-displaySortedNumbers(firstUser, secondUser, thirdUser)
+
+low, mid, hig = displaySortedNumbers(firstUser, secondUser, thirdUser)
+
+print("The sorted numbers are", low, mid, hig)
