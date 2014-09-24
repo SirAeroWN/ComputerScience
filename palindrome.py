602cac7b6f6421f9f4428fad8221e488d3229aab
Will Norvelle <willnorvelle@gmail.com>
2014-09-24 09:51:26 -0400

    can now find all palindromic prime numbers

--- palindrome.py
+++ palindrome.py
@@ -19,18 +19,47 @@ def reverse(number):
     return int(numStr)
 
 def isPrime(number):
-	divisor = 2
-	while(divisor <= (number / 2)):
-		if((number % divisor) == 0):
-			return False
-		divisor += 1
-	return True
+    divisor = 2
+    boolVal = True
+    while(divisor <= (number / 2)):
+        if((number % divisor) == 0):
+            boolVal = False
+        divisor += 1
+    return boolVal
 
 def findNumOfPrimes():
-	count = 0
-	for i in range(1, 10001):
-		if(isPrime(i)):
-			count += 1
-	return count
+    count = 0
+    for i in range(1, 10001):
+        #if(isPrime(i)):
+        if(isPalindrome(i)):
+            count += 1
+    return count
 
-print(findNumOfPrimes())
+def isPrimePalindrome(number):
+    boolVal = False
+    if(isPalindrome(number)):
+        if(isPrime(number)):
+            boolVal = True
+    return boolVal
+
+def formatPrint(unformStr):
+    print(format(unformStr, ">7s"), end = '')
+    return
+
+def findYoPalenPrimes():
+    count = 0
+    displayCount = 0
+    candidate = 1
+    while(count < 100):
+        if(isPrimePalindrome(candidate)):
+            formatPrint(str(candidate))
+            displayCount += 1
+            count += 1
+        if(displayCount == 10):
+            print("\n")
+            displayCount = 0
+        candidate += 1
+    return
+
+findYoPalenPrimes()
+
