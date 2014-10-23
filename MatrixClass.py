a1a13478d79ee08d37c1d20502620d407a716469
Will Norvelle <willnorvelle@gmail.com>
2014-10-22 21:17:45 -0400

    INVERSE

--- MatrixClass.py
+++ MatrixClass.py
@@ -3,9 +3,8 @@ class Matrix:
 		self.rows = rows
 		self.columns = columns
 		self.contents = []
-		width = [1] * columns
 		for i in range(0, rows): #in form matrix[rows][columns]
-			self.contents.append(width)
+			self.contents.append([0] * columns)
 		return
 
 	def printit(self):
@@ -14,10 +13,79 @@ class Matrix:
 
 	def assignRow(self, row, contentList):
 		if(len(contentList) == len(self.contents[row])):
-			for i in (0, len(self.contents[row])):
+			upper = len(self.contents[row])
+			i = 0
+			while(i < upper):
 				self.contents[row][i] = contentList[i]
+				i += 1
 		else:
 			print("ERROR: in assignRow: List is not same size as matrix")
+
+	def augmentIdentity(self):
+		augmented = Matrix(self.rows, (self.columns * 2))
+		for i in range(0, self.rows):
+			newRow = self.contents[i]
+			for j in range(0, i):
+				newRow.append(0)
+			newRow.append(1)
+			for k in range(0, (self.rows - i) - 1):
+				newRow.append(0)
+			augmented.assignRow(i, newRow)
+		self.columns = self.columns * 2
+		return augmented
+
+	def rowMul(self, row, scalar):
+		alterMatrix = self
+		for i in range(0, alterMatrix.columns):
+			alterMatrix.contents[row][i] = alterMatrix.contents[row][i] * scalar
+		return alterMatrix
+
+	def rowDiv(self, row, scalar):
+		alteredMatrix = self
+		for i in range(0, alteredMatrix.columns):
+			alteredMatrix.contents[row][i] = alteredMatrix.contents[row][i] / scalar
+		return alteredMatrix
+
+	def rowAdd(self, first, second):
+		alteredMatrix = self
+		for i in range(0, alteredMatrix.columns):
+			alteredMatrix.contents[second][i] = alteredMatrix.contents[second][i] + alteredMatrix.contents[first][i]
+		return alteredMatrix
+
+	def rowCombine(self, scalarRow1, MulDiv, row1, row2):
+		propMatrix = self
+		if(MulDiv == 1):
+			propMatrix.rowMul(row1, scalarRow1)
+		else:
+			propMatrix.rowDiv(row1, scalarRow1)
+		propMatrix.rowAdd(row1, row2)
+		if(MulDiv == 1):
+			propMatrix.rowDiv(row1, scalarRow1)
+		else:
+			propMatrix.rowMul(row1, scalarRow1)
+		return propMatrix
+
+	def rref(self):
+		#get bottom triangle of zeros
+		for k in range(0, self.rows):
+			for i in range(k, self.rows):
+				self.rowDiv(i, self.contents[i][k])
+			for j in range(k + 1, self.rows):
+				self.rowCombine(-1, 1, k, j)
+
+		#get top triangle of zeros
+		for i in range(0, (self.rows) - 1):
+			print("row", i, self.contents[i])
+			for j in range((i + 1), self.rows):
+				if(self.contents[i][j] != 0):
+					self.rowCombine((-1 * self.contents[i][j]), 1, j, i)
+		return
+
+	def stripFront(self):
+		stripped = Matrix(self.rows, (self.columns / 2))
+		for i in range(self.rows, self.columns):
+			stripped.contents[]
+
 
 	def __add__(self, other):
 		newMatrix = Matrix()
@@ -25,16 +93,17 @@ class Matrix:
 			for col in range(0, self.columns):
 				newMatrix.contents[row][col] = self.contents[row][col] + other.contents[row][col]
 		return newMatrix
+
 #
 #	def assignAll(self, contentList):
 #		if((len(contentList[]) == self.__rows) and (len(contentList[0]):
 #			for i in range(0, self.__rows):
 
-a = Matrix()
-#a.assignRow(0, [1,2])
-a.printit()
-b = Matrix()
-#b.assignRow(1, [1,2])
-b.printit()
-c = a + b
-c.printit()
+# a = Matrix()
+# a.assignRow(0, [1,2])
+# a.printit()
+# b = Matrix()
+# b.assignRow(1, [1,2])
+# b.printit()
+# c = a + b
+# c.printit()
---
+++ MatrixInverse.py
@@ -0,0 +1,18 @@
+from MatrixClass import Matrix
+
+# a = Matrix()
+# a.assignRow(0, [4,3])
+# a.assignRow(1, [3,2])
+# a.augmentIdentity()
+# a.rref()
+# a.printit()
+
+b = Matrix(3, 3)
+b.assignRow(0, [1,4,-3])
+b.assignRow(1, [-2,-7,6])
+b.assignRow(2, [1,7,-2])
+#b.printit()
+b.augmentIdentity()
+#b.printit()
+b.rref()
+b.printit()
