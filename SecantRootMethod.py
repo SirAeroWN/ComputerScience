4214abfadad62413915025c29bfbf0c03bd6bdc3
Will Norvelle <willnorvelle@gmail.com>
2015-04-01 08:39:40 -0400

    added

--- SecantRootMethod.py
+++ SecantRootMethod.py
@@ -21,16 +21,25 @@ def secantRootMethod(x0, x1, c, x):
 		print(root(x0, x1))
 	return
 
+def update(val):
+	secantRootMethod(leftSlider.val, rightSlider.val, 0, np.linspace(-4.0, 4.0, 100000))
+	figure.canvas.draw_idle()
+	return
+
 figure, ax = plt.subplots()
 plt.subplots_adjust(left = 0.25, bottom = 0.25)
 x = np.linspace(-4.0, 4.0, 100000) # 100,000 x's
 y = f(x)
-x0 = 1
-x1 = 2
+axcolor = 'lightgoldenrodyellow'
 original, = plt.plot(x,y)
-secantRootMethod(x0, x1, 0, x)
 plt.axis([-4, 4, np.amin(y) - 1, np.amax(y) + 1])
 plt.grid(True)
 plt.axhline(0, color='black', lw=2)
 plt.axvline(0, color='black', lw=2)
+x0ax = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+x1ax  = plt.axes([0.25, 0.15, 0.65, 0.03], axisbg=axcolor)
+leftSlider = Slider(x0ax, 'Left Boundry', -3, 3, valinit = 1)
+rightSlider = Slider(x1ax, 'Right Boundry', -3, 3, valinit = 2)
+leftSlider.on_changed(update)
+rightSlider.on_changed(update)
 plt.show()
