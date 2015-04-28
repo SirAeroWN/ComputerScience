d3b5c4dc40e183c772c1afabd221368118e7666b
Will Norvelle <willnorvelle@gmail.com>
2015-04-28 07:38:02 -0400

    rearranged

--- 101/December/BouncingBalls.py
+++ 101/December/BouncingBalls.py
@@ -18,11 +18,11 @@ def toHexChar(hexValue):
 # Define a Ball class
 class Ball:
     def __init__(self, yspeed):
-        self.x = 0 # Starting center position
-        self.y = 0 
-        self.dx = 2#1.0 / (1 + yspeed) # Move right by default
-        self.dy = 2#yspeed # Move down by default
-        self.radius = 6 # The radius is fixed
+        self.x = 175 # Starting center position
+        self.y = 87 
+        self.dx = 0 # 1.0 / (1 + yspeed) # Move right by default
+        self.dy = 0 # yspeed # Move down by default
+        self.radius = 10 # The radius is fixed
         self.color = getRandomColor() # Get random color
 
 class BounceBalls:
@@ -34,21 +34,20 @@ class BounceBalls:
         
         self.width = 350 # Width of the self.canvas
         self.height = 350 / 2 # Width of the self.canvas
-        self.canvas = Canvas(window, bg = "white", 
-            width = self.width, height = self.height)
+        self.canvas = Canvas(window, bg = "white", width = self.width, height = self.height)
         self.canvas.pack()
         
         frame = Frame(window)
         frame.pack()
         btStop = Button(frame, text = "Stop", command = self.stop)
         btStop.pack(side = LEFT)
-        btResume = Button(frame, text = "Resume",
-            command = self.resume)
+        btResume = Button(frame, text = "Resume", command = self.resume)
         btResume.pack(side = LEFT)
-        btAdd = Button(frame, text = "+", command = self.add)
-        btAdd.pack(side = LEFT)
-        btRemove = Button(frame, text = "-", command = self.remove)
-        btRemove.pack(side = LEFT)
+        self.add()
+        # btAdd = Button(frame, text = "+", command = self.add)
+        # btAdd.pack(side = LEFT)
+        # btRemove = Button(frame, text = "-", command = self.remove)
+        # btRemove.pack(side = LEFT)
         
         self.sleepTime = 100 # Set a sleep time 
 
@@ -56,6 +55,14 @@ class BounceBalls:
         FasterBtn.pack(side = LEFT)
         SlowerBtn = Button(frame, text = "Slower", command = self.slow)
         SlowerBtn.pack(side = LEFT)
+        LeftBtn = Button(frame, text = "Left", command = self.left)
+        LeftBtn.pack(side = LEFT)
+        RightBtn = Button(frame, text = "Right", command = self.right)
+        RightBtn.pack(side = LEFT)
+        UpBtn = Button(frame, text = "Up", command = self.up)
+        UpBtn.pack(side = LEFT)
+        DownBtn = Button(frame, text = "Down", command = self.down)
+        DownBtn.pack(side = LEFT)
         self.isStopped = False
         self.animate()
         
@@ -86,6 +93,26 @@ class BounceBalls:
     
     def remove(self): # Remove the last ball 
         self.ballList.pop()
+        return
+
+    def left(self):
+        for ball in self.ballList:
+            ball.dx -= 1
+        return
+
+    def right(self):
+        for ball in self.ballList:
+            ball.dx += 1
+        return
+
+    def up(self):
+        for ball in self.ballList:
+            ball.dy -= 1
+        return
+
+    def down(self):
+        for ball in self.ballList:
+            ball.dy += 1
         return
                                 
     def animate(self): # Move the message
